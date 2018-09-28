9a36f675ee434b800ca925ab186e52b1ae1bbcb9
clifford <clifford@noukod.com>
2018-09-28 19:49:33 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,11 @@
 def introduction(name)
+<<<<<<< HEAD
   puts "Hi, my name is #{name}."
 end
 
  def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+=======
+
+>>>>>>> 2c705843b763872c8b259f86756b44611843b575
 end
